9f8f8798e989216d163b5c9432f389928983ef94
tim <tim.galvin@curtin.edu.au>
2021-08-08 17:45:45 +0800

    check for existing aegean comp

--- inject_sources.sh
+++ inject_sources.sh
@@ -74,7 +74,8 @@ input_map_comp="${input_map_dir}/${imageset_name}_psf_comp.fits"
 input_map_rms="${input_map_dir}/${imageset_name}_rms.fits"
 input_map_bkg="${input_map_dir}/${imageset_name}_bkg.fits"
 input_map_psf="${input_map_dir}/${imageset_name}_projpsf_psf.fits"
-for file in "${input_map}" "${input_map_rms}" "${input_map_bkg}" "${input_map_psf}" "${input_sources}"; do
+
+for file in "${input_map}" "${input_map_comp}" "${input_map_rms}" "${input_map_bkg}" "${input_map_psf}" "${input_sources}"; do
     if [ ! -e "${file}" ]; then
         echo "Error: $file does not exist. Aborting."
         exit 1
